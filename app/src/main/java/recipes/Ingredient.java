e2bd722921567d86d31afdcfee864cc5d728c1a2
jaddev888@gmail.com <denis777>
2018-04-15 13:30:12 +0300

    create project

---
+++ app/src/main/java/recipes/Ingredient.java
@@ -0,0 +1,43 @@
+
+package recipes;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient {
+
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+}
